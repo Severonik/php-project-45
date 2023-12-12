bd645053b497bf476cd486f5511f587420d07269
Severonik <severov_1996@mail.ru>
2023-12-12 21:19:19 +0300

    Исправлен Gsd

--- src/Games/GcdGame.php
+++ src/Games/GcdGame.php
@@ -15,14 +15,15 @@ function gcdGame()
 {
     return [
         'type' => 'gcd',
-        'question' => function () {
+        'generateQuestion' => function () {
             $number1 = mt_rand(1, 100);
             $number2 = mt_rand(1, 100);
+
             return "$number1 $number2";
         },
-        'correctAnswer' => function ($question) {
+        'calculateCorrectAnswer' => function ($question) {
             list($number1, $number2) = explode(' ', $question);
             return gcd($number1, $number2);
-        },
+        }
     ];
 }
