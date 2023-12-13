a7a082ada22c488359eb6762214c435cae32b404
Severonik <severov_1996@mail.ru>
2023-12-13 04:27:22 +0300

    test

--- src/Games/ProgressionGame.php
+++ src/Games/ProgressionGame.php
@@ -1,49 +1,55 @@
 <?php
 
-function generateProgression()
-{
-    $length = mt_rand(5, 10);
-    $start = mt_rand(1, 20);
-    $step = mt_rand(1, 5);
-
-    $progression = [$start];
-
-    for ($i = 1; $i < $length; $i++) {
-        $progression[] = $start + $i * $step;
-    }
-
-    return $progression;
-}
-
-function replaceWithDots($progression, $position)
-{
-    $progressionWithDots = $progression;
-    $progressionWithDots[$position] = '..';
-
-    return $progressionWithDots;
-}
-
 function progressionGame()
 {
-    return [
-        'type' => 'progression',
-        'generateQuestion' => function () {
-            $progression = generateProgression();
-            $hiddenPosition = mt_rand(0, count($progression) - 1);
+    return new class implements Game {
+        private $progression;
+        private $hiddenPosition;
 
-            $progressionWithDots = replaceWithDots($progression, $hiddenPosition);
+        public function getType()
+        {
+            return 'progression';
+        }
 
-            return [
-                'question' => implode(' ', $progressionWithDots),
-                'hiddenPosition' => $hiddenPosition,
-                'hiddenNumber' => $progression[$hiddenPosition],
-            ];
-        },
-        'calculateCorrectAnswer' => function ($questionData) {
-            return strval($questionData['hiddenNumber']);
-        },
-        'askUserAnswer' => function () {
-            return readline("Your answer: ");
-        },
-    ];
+        public function generateQuestion()
+        {
+            $this->progression = generateProgression();
+            $this->hiddenPosition = mt_rand(0, count($this->progression) - 1);
+
+            $progressionWithDots = replaceWithDots($this->progression, $this->hiddenPosition);
+
+            return implode(' ', $progressionWithDots);
+        }
+
+        public function calculateCorrectAnswer($question)
+        {
+            $progression = $this->progression;
+            $hiddenNumber = $progression[$this->hiddenPosition];
+
+            return $hiddenNumber;
+        }
+
+        private function generateProgression()
+        {
+            $length = mt_rand(5, 10);
+            $start = mt_rand(1, 20);
+            $step = mt_rand(1, 5);
+
+            $progression = [$start];
+
+            for ($i = 1; $i < $length; $i++) {
+                $progression[] = $start + $i * $step;
+            }
+
+            return $progression;
+        }
+
+        private function replaceWithDots($progression, $position)
+        {
+            $progressionWithDots = $progression;
+            $progressionWithDots[$position] = '..';
+
+            return $progressionWithDots;
+        }
+    };
 }
