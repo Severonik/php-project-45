d28b95cacd3e496553503dff4d055c42440a8c15
Severonik <severov_1996@mail.ru>
2023-12-13 06:43:20 +0300

    тест

--- src/Cli.php
+++ src/Cli.php
@@ -43,7 +43,7 @@ function showGameInstructions(string $gameType = '')
     }
 }
 
-function showQuestion(array $question)
+function showQuestion($question)
 {
     if (is_array($question)) {
         $questionText = implode(' ', $question);
--- src/Game.php
+++ src/Game.php
@@ -12,5 +12,14 @@ function generateQuestionFunction(array $game): string
 
 function calculateCorrectAnswerFunction(array $game, string $question)
 {
-    return $game['calculateCorrectAnswer']($question);
+    return $game['calculateCorrectAnswer'](extractQuestionData($question));
+}
+
+function extractQuestionData(string $question): array
+{
+    // Разбираем вопрос и извлекаем необходимые данные
+    // Например, если вопрос "2 5", извлекаем [2, 5]
+    $questionData = array_map('intval', explode(' ', $question));
+
+    return $questionData;
 }
