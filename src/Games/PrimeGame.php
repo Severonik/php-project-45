3203a29fe9400dc1aa4b6923dd5a4a739dcf544b
Severonik <severov_1996@mail.ru>
2023-12-13 04:46:45 +0300

    Prime

--- src/Games/PrimeGame.php
+++ src/Games/PrimeGame.php
@@ -1,5 +1,20 @@
 <?php
 
+function isPrime($number)
+{
+    if ($number < 2) {
+        return false;
+    }
+
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function primeGame()
 {
     return [
