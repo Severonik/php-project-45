dce69658d0d4125121fe39db6980c315edfabb24
Severonik <severov_1996@mail.ru>
2023-12-13 04:37:53 +0300

    test

--- src/Games/ProgressionGame.php
+++ src/Games/ProgressionGame.php
@@ -1,52 +1,48 @@
 <?php
 
+function generateProgression()
+{
+    $length = mt_rand(5, 10);
+    $start = mt_rand(1, 20);
+    $step = mt_rand(1, 5);
+
+    $progression = [$start];
+
+    for ($i = 1; $i < $length; $i++) {
+        $progression[] = $start + $i * $step;
+    }
+
+    return $progression;
+}
+
+function replaceWithDots($progression, $position)
+{
+    $progressionWithDots = $progression;
+    $progressionWithDots[$position] = '..';
+
+    return $progressionWithDots;
+}
+
 function progressionGame()
 {
+    return [
+        'type' => 'progression',
+        'generateQuestion' => function () {
+            global $progression, $hiddenPosition;
 
-
-        function getType()
-        {
-            return 'progression';
-        }
-
-        function generateQuestion()
-        {
-            $this->progression = generateProgression();
-            $this->hiddenPosition = mt_rand(0, count($this->progression) - 1);
-
-            $progressionWithDots = replaceWithDots($this->progression, $this->hiddenPosition);
-
+            $progression = generateProgression();
+            $hiddenPosition = mt_rand(0, count($progression) - 1);
+        
+            $progressionWithDots = replaceWithDots($progression, $hiddenPosition);
+        
             return implode(' ', $progressionWithDots);
-        }
+        },
+        'calculateCorrectAnswer' => function ($question) {
+            global $progression, $hiddenPosition;
 
-        function calculateCorrectAnswer($question)
-        {
-            $progression = $this->progression;
-            $hiddenNumber = $progression[$this->hiddenPosition];
-
+            $hiddenNumber = $progression[$hiddenPosition];
+        
             return $hiddenNumber;
         }
-
-        function generateProgression()
-        {
-            $length = mt_rand(5, 10);
-            $start = mt_rand(1, 20);
-            $step = mt_rand(1, 5);
-
-            $progression = [$start];
-
-            for ($i = 1; $i < $length; $i++) {
-                $progression[] = $start + $i * $step;
-            }
-
-            return $progression;
-        }
-
-        function replaceWithDots($progression, $position)
-        {
-            $progressionWithDots = $progression;
-            $progressionWithDots[$position] = '..';
-
-            return $progressionWithDots;
-        }
+    ];
 }
