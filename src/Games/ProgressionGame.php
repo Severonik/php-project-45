3ef41ad4e181d02e9d67a3c41abd7546eb6a51b4
Severonik <severov_1996@mail.ru>
2023-12-13 04:22:36 +0300

    !!!!!!

--- src/Games/ProgressionGame.php
+++ src/Games/ProgressionGame.php
@@ -25,39 +25,25 @@ function replaceWithDots($progression, $position)
 
 function progressionGame()
 {
-    $gameData = generateQuestion();
-
     return [
         'type' => 'progression',
-        'question' => $gameData['question'],
-        'correctAnswer' => $gameData['correctAnswer'],
-        'checkAnswer' => function ($userAnswer) use ($gameData) {
-            checkAnswer($userAnswer, $gameData['correctAnswer']);
+        'generateQuestion' => function () {
+            $progression = generateProgression();
+            $hiddenPosition = mt_rand(0, count($progression) - 1);
+
+            $progressionWithDots = replaceWithDots($progression, $hiddenPosition);
+
+            return [
+                'question' => implode(' ', $progressionWithDots),
+                'hiddenPosition' => $hiddenPosition,
+                'hiddenNumber' => $progression[$hiddenPosition],
+            ];
+        },
+        'calculateCorrectAnswer' => function ($questionData) {
+            return strval($questionData['hiddenNumber']);
+        },
+        'askUserAnswer' => function () {
+            return readline("Your answer: ");
         },
     ];
-}
-
-function generateQuestion()
-{
-    $progression = generateProgression();
-    $hiddenPosition = mt_rand(0, count($progression) - 1);
-
-    $progressionWithDots = replaceWithDots($progression, $hiddenPosition);
-
-    $question = implode(' ', $progressionWithDots);
-    $correctAnswer = strval($progression[$hiddenPosition]);
-
-    return [
-        'question' => $question,
-        'correctAnswer' => $correctAnswer,
-    ];
-}
-
-function checkAnswer($userAnswer, $correctAnswer)
-{
-    if ($userAnswer === $correctAnswer) {
-        print("Correct!\n");
-    } else {
-        print("'$userAnswer' is wrong answer ;(. Correct answer was '$correctAnswer'.\n");
-    }
 }
